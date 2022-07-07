38b73742758d8aa1ad2fb5ba059962ff2ddffb34
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-07-07 07:03:51 +0530

    reverse of a number

--- Conditionals-and-Loops/src/com/kunal/reverseNumber.java
+++ Conditionals-and-Loops/src/com/kunal/reverseNumber.java
@@ -1,4 +1,14 @@
 package com.kunal;
 
 public class reverseNumber {
+    public static void main(String[] args) {
+        int n = 23597;
+        int reverse = 0;
+        while(n > 0){
+            int rem = n % 10;
+            reverse = reverse * 10 + rem;
+            n = n / 10;
+        }
+        System.out.println(reverse);
+    }
 }
