ddc2582cfc1521a9127b3c5e38955c01e4bd4396
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-07-07 07:45:00 +0530

    area of triangle

--- Conditionals-and-Loops/src/com/kunal/asssignments/basic/triangleArea.java
+++ Conditionals-and-Loops/src/com/kunal/asssignments/basic/triangleArea.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class triangleArea {
     public static void main(String[] args) {
-        Scanner in = new Scanner()
+        Scanner in = new Scanner(System.in);
+        System.out.print("Base of the Triangle: ");
+        int base = in.nextInt();
+        System.out.print("Height of the Triangle: ");
+        int height = in.nextInt();
+        int area = (int)(0.5f * base * height);
+        System.out.println("Area of the Triangle is: " + area);
     }
 }
